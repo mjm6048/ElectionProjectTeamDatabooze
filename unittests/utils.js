d43740ab02fdd823300003aab7b98e390237f7b6
Michael McIntosh <mjm6048@rit.edu>
2024-02-24 22:58:07 -0500

    Renamed folders and simplified structure

--- unittests/utils.js
+++ unittests/utils.js
@@ -1,43 +1,35 @@
-
-
-  
-function isValidBallot(ballot)
-{
-   const errors =[];
-   if(ballot.name==null || ballot.description == null || ballot.startDate == null || ballot.endDate == null || ballot.societyID == null)
-   {
-    return false;
-   }
-   // Fields cannot be empty string
-   if(/^\s*$/.test(ballot.name) || /^\s*$/.test(ballot.description)|| /^\s*$/.test(ballot.startDate) || /^\s*$/.test(ballot.endDate))
-   {
-    return false;
-   }
-   // start date has to be less than end date
-   var sd = Date.parse(ballot.startDate);
-   var ed = Date.parse(ballot.endDate);
-   if(sd>ed)
-   {
-    return false;
-   }
-   //societyID has to be int
-   if(!(/^[0-9]+$/.test(ballot.societyID)))
-  {
-   return false;
-  }
-  return true;
+function isValidBallot(ballot) {
+    const errors = [];
+    if (ballot.name == null || ballot.description == null || ballot.startDate == null || ballot.endDate == null || ballot.societyID == null) {
+        return false;
+    }
+    // Fields cannot be empty string
+    if (/^\s*$/.test(ballot.name) || /^\s*$/.test(ballot.description) || /^\s*$/.test(ballot.startDate) || /^\s*$/.test(ballot.endDate)) {
+        return false;
+    }
+    // start date has to be less than end date
+    var sd = Date.parse(ballot.startDate);
+    var ed = Date.parse(ballot.endDate);
+    if (sd > ed) {
+        return false;
+    }
+    //societyID has to be int
+    if (!(/^[0-9]+$/.test(ballot.societyID))) {
+        return false;
+    }
+    return true;
 
 }
 
-function isValidCandidate(candidate){
-    if(/^\s*$/.test(candidate.username) || /^\s*$/.test(candidate.titles) || /^\s*$/.test(candidate.candidateDescription) || /^\s*$/.test(candidate.photo)){
+function isValidCandidate(candidate) {
+    if (/^\s*$/.test(candidate.username) || /^\s*$/.test(candidate.titles) || /^\s*$/.test(candidate.candidateDescription) || /^\s*$/.test(candidate.photo)) {
         return false;
-    }else{
+    } else {
         return true;
     }
 }
 
-export{
+export {
     isValidBallot,
     isValidCandidate
 }

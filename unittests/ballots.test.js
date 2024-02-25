06979ef77648c72f69634d367ff000ab894d6c7c
Michael McIntosh <mjm6048@rit.edu>
2024-02-24 23:02:45 -0500

    Fixed formatting and spelling

--- unittests/ballots.test.js
+++ unittests/ballots.test.js
@@ -1,35 +1,33 @@
 import * as utils from './utils.js';
 
-
-
-test(`Name cannot be null or empty spaces`,()=>{
-const result = utils.isValidBallot({name:" ", description: "dfsdfgsd",startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12});
+test(`Name cannot be null or empty spaces`, () => {
+    const result = utils.isValidBallot({ name: " ", description: "dfsdfgsd", startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12 });
     expect(result).toBe(false);
 
- });
-test(`Description cannot be null or empty spaces`,()=>{
-    const result = utils.isValidBallot({name:"dfgdsfg", description: "   ",startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12});
-        expect(result).toBe(false);
-    
-     });
-test(`startdate cannot be null or empty spaces`,()=>{
-const result = utils.isValidBallot({name:"dfgdsfg", description: "fdgf", endDate: "2.10.2024", societyID: 12});
+});
+test(`Description cannot be null or empty spaces`, () => {
+    const result = utils.isValidBallot({ name: "dfgdsfg", description: "   ", startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12 });
     expect(result).toBe(false);
 
-    });
-
-test(`societyID has to be an integer`,()=>{
-    const result = utils.isValidBallot({name:"dfgdsfg", description: "dfdf",startDate: "1.10.2024", endDate: "2.10.2024", societyID: "fddsf"});
+});
+test(`startdate cannot be null or empty spaces`, () => {
+    const result = utils.isValidBallot({ name: "dfgdsfg", description: "fdgf", endDate: "2.10.2024", societyID: 12 });
     expect(result).toBe(false);
-            
- });
-test(`Start date cannot be after end date`,()=>{
-    const result = utils.isValidBallot({name:"election 2024", description: "dfsdfgsd",startDate: "03.10.2024", endDate: "02.10.2024", societyID: 12});
-        expect(result).toBe(false);
-    
-    });
-    test(`Valid Ballot`,()=>{
-        const result = utils.isValidBallot({name:"election 2024", description: "dfsdfgsd",startDate: "01.10.2024", endDate: "02.10.2024", societyID: 12});
-            expect(result).toBe(true);
-        
-        });
+
+});
+
+test(`societyID has to be an integer`, () => {
+    const result = utils.isValidBallot({ name: "dfgdsfg", description: "dfdf", startDate: "1.10.2024", endDate: "2.10.2024", societyID: "fddsf" });
+    expect(result).toBe(false);
+
+});
+test(`Start date cannot be after end date`, () => {
+    const result = utils.isValidBallot({ name: "election 2024", description: "dfsdfgsd", startDate: "03.10.2024", endDate: "02.10.2024", societyID: 12 });
+    expect(result).toBe(false);
+
+});
+test(`Valid Ballot`, () => {
+    const result = utils.isValidBallot({ name: "election 2024", description: "dfsdfgsd", startDate: "01.10.2024", endDate: "02.10.2024", societyID: 12 });
+    expect(result).toBe(true);
+
+});
--- unittests/candidate.test.js
+++ unittests/candidate.test.js
@@ -1,27 +1,27 @@
 import * as utils from './utils.js';
 
-// positave test will good data
-test(`candidate must be able to be created when populated`,()=>{
-    const result = utils.isValidCandidate({username:"Godfree McMannish", titles: "Mr(s) President", candidateDescription:"A very nice person who will give free money to all", photo:"./images/candidates/cutepic.jpg"});
+// positive test will good data
+test(`candidate must be able to be created when populated`, () => {
+    const result = utils.isValidCandidate({ username: "Godfree McMannish", titles: "Mr(s) President", candidateDescription: "A very nice person who will give free money to all", photo: "./images/candidates/cutepic.jpg" });
     expect(result).toBe(true);
-    });
-// negitave test for empty username
-test(`candidate username can not be empty`,()=>{
-    const result = utils.isValidCandidate({username:"", titles: "Mr(s) President", candidateDescription:"A very nice person who will give free money to all", photo:"./images/candidates/cutepic.jpg"});
-        expect(result).toBe(false);
-    });
-//negitave test for empty title
-test(`candidate title can not be empty`,()=>{
-    const result = utils.isValidCandidate({username:"Godfree McMannish", titles: "", candidateDescription:"A very nice person who will give free money to all", photo:"./images/candidates/cutepic.jpg"});
-        expect(result).toBe(false);
-    });
-//negitave test for empty candidateDescription
-test(`candidateDescription can not be empty`,()=>{
-    const result = utils.isValidCandidate({username:"Godfree McMannish", titles: "Mr(s) President", candidateDescription:"", photo:"./images/candidates/cutepic.jpg"});
-        expect(result).toBe(false);
-    });
-//negitave test for empty candidate photo
-test(`candidate photo can not be empty`,()=>{
-    const result = utils.isValidCandidate({username:"Godfree McMannish", titles: "Mr(s) President", candidateDescription:"A very nice person who will give free money to all", photo:""});
-        expect(result).toBe(false);
-    });
+});
+// negative test for empty username
+test(`candidate username can not be empty`, () => {
+    const result = utils.isValidCandidate({ username: "", titles: "Mr(s) President", candidateDescription: "A very nice person who will give free money to all", photo: "./images/candidates/cutepic.jpg" });
+    expect(result).toBe(false);
+});
+// negative test for empty title
+test(`candidate title can not be empty`, () => {
+    const result = utils.isValidCandidate({ username: "Godfree McMannish", titles: "", candidateDescription: "A very nice person who will give free money to all", photo: "./images/candidates/cutepic.jpg" });
+    expect(result).toBe(false);
+});
+// negative test for empty candidateDescription
+test(`candidateDescription can not be empty`, () => {
+    const result = utils.isValidCandidate({ username: "Godfree McMannish", titles: "Mr(s) President", candidateDescription: "", photo: "./images/candidates/cutepic.jpg" });
+    expect(result).toBe(false);
+});
+// negative test for empty candidate photo
+test(`candidate photo can not be empty`, () => {
+    const result = utils.isValidCandidate({ username: "Godfree McMannish", titles: "Mr(s) President", candidateDescription: "A very nice person who will give free money to all", photo: "" });
+    expect(result).toBe(false);
+});

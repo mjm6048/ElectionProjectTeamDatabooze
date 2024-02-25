390c8494ca88ec7e2e936700eb7100b4caaffdeb
Michael McIntosh <mjm6048@rit.edu>
2024-02-24 23:00:08 -0500

    Fixed import

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import * as utils from './unittests/utils.js';
+import * as utils from './UnitTests/utils.js';
 console.log("hi");
 console.log(utils.isValidBallot({name:" ", description: "dfsdfgsd",startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12}));

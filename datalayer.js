03b1c012367ace939e498d4e4a7cba6d7c48a08b
Netra Krishnan <netra.kris@gmail.com>
2024-03-11 11:47:12 -0400

    added api calls to index.js and created react frontend folder

---
+++ datalayer.js
@@ -0,0 +1,25 @@
+
+
+
+const userExists=(username,password)=>
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this should be the name of the function to check login, refer to index.js for return type and arguments
+module.exports = {
+    userExists
+}
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-import * as utils from './UnitTests/utils.js';
-console.log("hi");
-console.log(utils.isValidBallot({name:" ", description: "dfsdfgsd",startDate: "1.10.2024", endDate: "2.10.2024", societyID: 12}));
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+const dl = require('./datalayer');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.post("/users/login",async(req,res)=>{
+    const{username,password}=req.body;
+    check = dl.userExists(username, password);
+    try{
+        
+        if(check){
+            res.status(200).json("Login successful");
+        }
+        else{
+            res.status(400).json("Invalid credentials");
+        }
+
+    }
+    catch(e){
+        res.status(500).json("Internal server error");
+    }
+
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log("port connected");
+})

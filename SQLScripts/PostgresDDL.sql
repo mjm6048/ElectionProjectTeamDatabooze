7bb6ac1bf1283432a40654282d91b69b169cadf9
Michael M <mjm6048@rit.edu>
2024-03-18 15:13:39 -0400

    Changed varchar to 50 and increased passwordHash to 100

--- SQLScripts/PostgresDDL.sql
+++ SQLScripts/PostgresDDL.sql
@@ -16,24 +16,24 @@ CREATE DATABASE databooze
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-    username varchar(30) NOT NULL PRIMARY KEY,
-    firstName varchar(30) NOT NULL,
-    lastName varchar(30) NOT NULL,
-    passwordHash varchar(30) NOT NULL,
+    username varchar(50) NOT NULL PRIMARY KEY,
+    firstName varchar(50) NOT NULL,
+    lastName varchar(50) NOT NULL,
+    passwordHash varchar(100) NOT NULL,
     roleID int NOT NULL
 );
 
 DROP TABLE IF EXISTS society CASCADE;
 CREATE TABLE society(
     societyID INT NOT NULL PRIMARY KEY,
-    societyName varchar(50), 
+    societyName varchar(100), 
     societyDescription varchar(100)
 );
 
 DROP TABLE IF EXISTS ballots CASCADE;
 CREATE TABLE ballots(
     ballotID SERIAL NOT NULL PRIMARY KEY, 
-    ballotName varchar(30), 
+    ballotName varchar(50), 
     startDate DATE, 
     endDate DATE,
     societyID int,
@@ -45,7 +45,7 @@ CREATE TABLE ballots(
 DROP TABLE IF EXISTS position_ballots CASCADE;
 CREATE TABLE position_ballots( 
     positionID int NOT NULL PRIMARY KEY, 
-    positionName varchar(30), 
+    positionName varchar(50), 
     maxNumCandidates int, 
     numVotesAllowed int, 
     ballotID int,
@@ -56,12 +56,12 @@ CREATE TABLE position_ballots(
 
 DROP TABLE IF EXISTS candidate CASCADE;
 CREATE TABLE candidate(
-    username varchar(30) NOT NULL,
+    username varchar(50) NOT NULL,
     positionID int NOT NULL,
-    titles varchar(30),
+    titles varchar(50),
     candidateDescription varchar(100),
     writeIn boolean,
-    photo varchar(30),
+    photo varchar(150),
     CONSTRAINT candidate_key PRIMARY KEY (username, positionID),
     FOREIGN KEY (username) REFERENCES users (username),
     FOREIGN KEY (positionID) REFERENCES position_ballots (positionID)
@@ -71,7 +71,7 @@ DROP TABLE IF EXISTS votes CASCADE;
 CREATE TABLE votes( 
     voteID INT NOT NULL PRIMARY KEY, 
     votes JSON,
-    username varchar(30) NOT NULL,
+    username varchar(50) NOT NULL,
 	CONSTRAINT fk_username
     	FOREIGN KEY (username)
 		REFERENCES users (username)
@@ -80,7 +80,7 @@ CREATE TABLE votes(
 DROP TABLE IF EXISTS initiative_ballots CASCADE;
 CREATE TABLE initiative_ballots(
     initiativeID int PRIMARY KEY NOT NULL,
-    initiativeName varchar(30),
+    initiativeName varchar(50),
     initiativeDescription varchar(100), 
     numVotesAllowed int,
     ballotID int, 
@@ -92,7 +92,7 @@ CREATE TABLE initiative_ballots(
 
 DROP TABLE IF EXISTS users_society CASCADE;
 CREATE TABLE users_society(
-    username varchar(30) NOT NULL,
+    username varchar(50) NOT NULL,
     societyID INT NOT NULL,
     CONSTRAINT users_society_key PRIMARY KEY (username, societyID),
     FOREIGN KEY (username) REFERENCES users(username),

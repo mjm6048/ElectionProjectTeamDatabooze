c3f7899e90ed9be73c82fa48ab684d69661a793d
Netra Krishnan <netra.kris@gmail.com>
2024-03-26 13:32:25 -0400

    hashing password

--- businesslayer.js
+++ businesslayer.js
@@ -2,12 +2,15 @@
 const dl = require('./datalayer');
 const userExists = async(username,password)=>
 {
-   
+    hashedpassword = createHash('sha256').update(password)
+    console.log(hashedpassword);
         try{
       user = await dl.getUser(username);
      if(user.length !=0){
         
             if (user[0].passwordhash == password)
+            // uncomment below and delete above when you've figured out what password was used -Netra
+        // if (user[0].passwordhash == hashedpassword)
             { console.log(password);
                 return true;
             }

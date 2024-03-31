429b53e58e72cfdf010e6bc991719ee80c76ace1
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2024-03-31 16:19:23 -0400

    fixed formatting issues

--- datalayer.js
+++ datalayer.js
@@ -11,90 +11,72 @@ const pool = new Pool({
 
 // Get User
 const  getUser = async(username)=> {
-try{
-
+  try{
     const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-   
     return result.rows;
-}
-catch(error)
-{   console.log(error);
+  }catch(error){
+    console.log(error);
     throw error;
-}
-
   }
-const getMembersandOfficers = async(societyID)=>
-{
+}
+const getMembersandOfficers = async(societyID)=>{
   try{
     const result = await pool.query('SELECT * FROM users JOIN users_society ON users.username WHERE users_society.societyID = $1 AND users.roleID IN (1,2)', [societyID]);
     return result.rows;
-}
-catch(error)
-{   console.log(error);
+  }catch(error){
+    console.log(error);
     throw error;
+  }
 }
-}
-
 
 const  getCandidates = async(positionID, username)=> {
-    try{
+  try{
     if(positionID !== 0){
-        const result = await pool.query('SELECT * FROM candidate WHERE positionID = $1', [positionID]);
-    }
-    else
-    {
+      const result = await pool.query('SELECT * FROM candidate WHERE positionID = $1', [positionID]);
+    }else{
       const result = await pool.query('SELECT * FROM candidate WHERE username = $1', [username]);
-    } 
-        return result.rows;
     }
-    catch(error)
-    {   console.log(error);
-        throw error;
-    }
-    
-      }
-  const getVote=async(VoteID)=>
-  {
-      const result = await pool.query('SELECT * FROM votes WHERE voteID = $1', [voteID]);
+    return result.rows;
   }
-  const countPositionVotes= async(positonID,candidateID)=>
-  {
-    try{
-     
-      {
+  catch(error){
+    console.log(error);
+    throw error;
+  }
+}
+
+const getVote=async(VoteID)=> {
+  const result = await pool.query('SELECT * FROM votes WHERE voteID = $1', [voteID]);
+}
+
+const countPositionVotes= async(positonID,candidateID)=> {
+  try{
+    {
       if(candidateID == 0){
         const result = await pool.query(`SELECT COUNT(*) AS vote_count FROM votes WHERE votes->>'VoteType' = 'ballot' AND (votes->>'BallotID')::int = $1`, [positonID]);
-      }
-      else{
+      }else{
         const result = await pool.query(`SELECT COUNT(*) AS vote_count FROM votes WHERE votes->>'VoteType' = 'ballot' AND (votes->>'BallotID')::int = $1 AND (votes->>'VotedFor')::int = $2`, [positonID,candidateID]);
       }
     }
-          return result.rows;
-      }
-      catch(error)
-      {   console.log(error);
-          throw error;
-      }
+    return result.rows;
+  }catch(error){
+      console.log(error);
+      throw error;
+    }
+}
+
+const getInititativeVotes= async(voteID,initiativeID)=> {
+  try{
+    if(VoteID!==0){
+      const result = await pool.query('SELECT * FROM votes WHERE voteID = $1', [voteID]);
+    }else{
+      const result = await pool.query(`SELECT * FROM votes WHERE votes->>'VoteType' = 'inititative' AND (votes->>'BallotID')::int = $1`, [initiativeID]);
+    } 
+    return result.rows;
+  }catch(error){
+    console.log(error);
+    throw error;
   }
-  const getInititativeVotes= async(voteID,initiativeID)=>
-  {
-    try{
-      if(VoteID!==0)
-      {
-        const result = await pool.query('SELECT * FROM votes WHERE voteID = $1', [voteID]);
-      }
-      else
-      {
-        const result = await pool.query(`SELECT * FROM votes WHERE votes->>'VoteType' = 'inititative' AND (votes->>'BallotID')::int = $1`, [initiativeID]);
-      } 
-    
-          return result.rows;
-      }
-      catch(error)
-      {   console.log(error);
-          throw error;
-      }
-  }
+}
 
 
 

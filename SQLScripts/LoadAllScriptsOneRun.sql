09d53def2b8b6d08f8e5bce8566fcc215fc16dc2
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2024-04-05 19:16:06 -0400

    Super script to run all inserts -Michael

---
+++ SQLScripts/LoadAllScriptsOneRun.sql
@@ -0,0 +1,11 @@
+\ir PostgresDDL.sql
+\ir InsertSocietyData.sql
+\ir InsertUserData.sql
+\ir InsertUsers_SocietyData.sql
+\ir InsertBallotData.sql
+\ir InsertBallotItemData.sql
+\ir InsertCandidateData.sql
+\ir InsertVoteData.sql
+
+-- While inside psql
+-- \i /path/to/file/LoadAllScriptsOneRun.sql

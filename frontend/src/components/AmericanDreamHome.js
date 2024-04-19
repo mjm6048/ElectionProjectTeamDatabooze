d561a324d5512c8aa6985f196363bc5949e2fd78
Nicholas Howard-Fudge <52792556+tfudge1@users.noreply.github.com>
2024-04-19 11:23:34 -0400

    added fetch data for ADHome

--- frontend/src/components/AmericanDreamHome.js
+++ frontend/src/components/AmericanDreamHome.js
@@ -27,8 +27,23 @@ export default class AmericanDreamHome extends React.Component{
             </>
         )
     }
-    componentDidMount(){
+    async componentDidMount(){
         //runs when render is in the DOM
         //get all Soc 
+        try {
+            await axios.get("https://databooze-dev.webdev.gccis.rit.edu/societies")
+            .then(response=> { 
+                if (response.status === 200) {
+                    this.setState({
+                        societies:response
+                    });
+                } else {
+                    console.log(response.status);
+                    alert("Invalid credentials");
+                }
+           })
+    }catch(error){
+        alert("Error encountered while getting societies");
+    }
     }
 }
